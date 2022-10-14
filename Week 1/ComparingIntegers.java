38d6028a11b4cdc6a18a3d3f5db70d5b1149bd05
Ahmed <ahmedmo501296@gmail.com>
2022-10-14 18:08:13 +0100

    First commit

---
+++ Week 1/ComparingIntegers.java
@@ -0,0 +1,26 @@
+/*Write an application that asks the user to enter
+two integers, obtains them from the user and displays the larger number followed by the
+words "is larger". If the numbers are equal, print the message "These numbers are equal" */
+import java.util.Scanner;
+
+public class ComparingIntegers{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Integers 1");
+        int input1 = Integer.valueOf(scan.nextLine());
+
+        System.out.println("Integers 2");
+        int input2 = Integer.valueOf(scan.nextLine());
+
+        if(input1> input2){
+            System.out.println(input1 + " is larger " + input2);
+        } else if (input2 > input1){
+            System.out.println(input2 + " is larger " + input1);
+        } else{
+            System.out.println("These numbers are equal");
+        }
+        
+    }
+
+}
