8a97c342615d705e84dd5dd7229297650dea5249
Ahmed <ahmedmo501296@gmail.com>
2022-10-18 19:23:01 +0100

    Week 1 - Completed

---
+++ acrostic.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+public class acrostic {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+        while (true){
+            Scanner scan = new Scanner(System.in);
+            String value = scan.nextLine();
+            if(value.equals("")){
+                break;
+                completed
+            strings.add(value.charAt(0));
+            }
+        }
+
+        for(String i: strings){
+            System.out.println(i);
+        }
+    }
+    
+}
